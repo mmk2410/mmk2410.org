636d957be96e24633c95ea7f7162edcec027a79c
Marcel Kapfer <opensource@mmk2410.org>
2020-05-15 20:26:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mmk2410.org
+
+This is the source code of my website. It uses Hugo with ox-hugo.
---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clean aka remove public/ if it exists
+if [[ -d ./public/ ]]; then
+    rm -rf ./public/
+fi
+
+# Build the site using hugo
+hugo
+
+# Deploy using rsync
+rsync \
+    --archive \
+    --verbose \
+    --compress \
+    --chown=marcel:www-data \
+    --delete \
+    --progress \
+    public/ \
+    mmk2410.org:/var/www/mmk2410.org/
