2e4b7a93cec12d0e04f9f8805ae4749fa7fddb78
Marcel Kapfer <opensource@mmk2410.org>
2024-03-17 22:59:12 +0100

    👷 Add continuous deliverly using Forgejo

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+INFO="\033[0;30m\033[47m"
+NC="\033[0m"
+
+log() {
+    echo -e "${INFO} INFO ${NC} $1"
+}
+
+log "Pulling latest changes from Git repository..."
+git pull origin "$1"
+
+log "Installing new composer dependencies and update autoloader..."
+composer install --no-interaction --prefer-dist --optimize-autoloader
