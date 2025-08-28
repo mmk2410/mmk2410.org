bfe262b6fbe35251f68a3a205976184a4ed07365
Marcel Kapfer <opensource@mmk2410.org>
2025-08-28 21:51:50 +0200

    ✨ Newest scribbles on homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -8,8 +8,8 @@
 <?php snippet('layout', slots: true) ?>
 <?php slot() ?>
 <header>
-	<h1><?= $site->title() ?></h1>
-	<h3><?= $page->subtitle() ?></h3>
+  <h1><?= $site->title() ?></h1>
+  <h3><?= $page->subtitle() ?></h3>
 </header>
 
 <?= $page->text()->kirbytext() ?>
@@ -25,6 +25,22 @@
 <?php foreach ($page->external_menu()->toStructure() as $item): ?>
   <a class="btn" href="<?= $item->link()->toUrl() ?>"><?= $item->title() ?></a>
 <?php endforeach ?>
+
+<h2>Newest scribbles</h2>
+
+<p>My scribbles are my microblog. Random notes, mostly without any deeper meaning.</p>
+
+<div>
+  <?php
+    page('scribbles')->children()->listed()->flip()->limit(3)->map(
+      fn (ScribblePage $scribble) => snippet('scribble', ['scribble' => $scribble, 'singlePage' => false])
+    )
+  ?>
+</div>
+
+<div style="margin-top: 40px;">
+  <a class="btn" href="<?= page('scribbles')->url() ?>">More mindless drivel</a>
+</div>
 
 <h2>Latest Posts</h2>
 
